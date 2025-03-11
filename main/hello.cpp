41f5e9c05e05412674132dfa8457d56dd9ab45c7
deekshaj2003 <136250532+deekshaj2003@users.noreply.github.com>
2025-03-11 21:35:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "Added new webhook to check !!!";
+  cout << "Added new webhook ";
   return 0;
 }
